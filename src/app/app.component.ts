917c8bcb449ffd262bd2ed0468ba8f4b6e7e7735
Daniel Kuhrmeyer <daniel.kuhrmeyer@rizepoint.com>
2018-03-23 14:14:53 -0600

    adding updates to left nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('myAwesomeAnimation', [
+      state('small', style({
+        transform: 'scale(1)',
+      })),
+      state('large', style({
+        transform: 'scale(1.2)',
+      })),
+      transition('small <=> large', animate('300ms ease-in')),
+    ]),
+  ]
 })
 export class AppComponent {
+  state: 'small';
   title = 'app';
   panelOpenState: false;
   screenWidth: number;
--- src/app/icon-nav/icon-nav.component.ts
+++ src/app/icon-nav/icon-nav.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-icon-nav',
   templateUrl: './icon-nav.component.html',
-  styleUrls: ['./icon-nav.component.css']
+  styleUrls: ['./icon-nav.component.css'],
+  animations: [
+    trigger('myAwesomeAnimation', [
+      state('small', style({
+        transform: 'scale(1)',
+      })),
+      state('large', style({
+        transform: 'scale(1.2)',
+      })),
+      transition('small => large', animate('300ms ease-in')),
+    ]),
+  ]
 })
 export class IconNavComponent implements OnInit {
 
